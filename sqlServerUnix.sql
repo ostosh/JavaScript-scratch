4db87110c565c42c8d731f05162bccec60d13ba2
Geoffrey Ostosh <ostoshg@gmail.com>
2016-09-21 19:52:31 -0700

    add unix timestamp conversion back to datetime

--- sqlServerUnix.sql
+++ sqlServerUnix.sql
@@ -4,9 +4,22 @@ DECLARE
   @dayInMills bigint = 60 * 60 * 24 * 1000,
   @unixTimestamp varchar(13)
 ;
+
 SET @
   unixTimestamp = (@dayInMills * DATEDIFF(DAY, @start, @end)) - 
     DATEDIFF(millisecond, @end, CAST(@end AS date));
 
+-- select unix timestamp in mills
+SET @unixTimestamp = (SELECT
+  RIGHT(CONCAT('000', @unixTimestamp), LEN(@unixTimestamp) + 3));
+  
+DECLARE @start datetime = '1970-01-01T00:00:00.000',
+  	@dayInMills bigint = 60 * 60 * 24 * 1000,
+    @unixTimestamp bigint
+;
+
+-- select datetime
 SELECT
-  RIGHT(CONCAT('000', @unixTimestamp), LEN(@unixTimestamp) + 3)
+  DATEADD(millisecond, @unixTimestamp % @dayInMills,
+  DATEADD(DAY, @unixTimestamp / @dayInMills, @start)
+  )
